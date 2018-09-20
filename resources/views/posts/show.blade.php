d733035d4c7001d53474918db5923622c553ad9b
Bart den Hoed <bartdhoed@gmail.com>
2018-09-21 00:38:19 +0200

    Add Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,10 @@
 
       {{ $post->body }}
 
-      <hr>
 
       @if (count($post->comments))
+          <hr>
+
           <div class="comments">
               <ul class="list-group">
                   @foreach ($post->comments as $comment)
@@ -23,6 +24,26 @@
               </ul>
           </div>
       @endif
+
+      <hr>
+
+      <div class="card">
+          <div class="card-block">
+              <form method="POST" action="/posts/{{ $post->id }}/comments">
+                  @csrf
+
+                  <div class="form-group">
+                      <textarea name="body" rows="4" placeholder="Your comment here." class="form-control" required></textarea>
+                  </div>
+
+                  <div class="form-group">
+                      <button type="submit" class="btn btn-primary">Add</button>
+                  </div>
+              </form>
+
+              @include('layouts.errors')
+          </div>
+      </div>
 
     </div><!-- /.blog-main -->
 @endsection
